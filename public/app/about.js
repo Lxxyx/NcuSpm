f181a8b426d1e2b98ed0aa8509e63779d13008ec
Lxxyx <841380530@qq.com>
2016-01-06 17:16:28 +0800

    start to design another index

--- public/app/about.js
+++
@@ -1,9 +0,0 @@
-var About = require('./components/about.vue');
-var Vue = require('vue');
-
-var vm = new Vue({
-	el: "body",
-	components:{
-		about:About
-	}
-});
--- public/app/index.js
+++
@@ -1,9 +0,0 @@
-var Index = require('./components/index.vue');
-var Vue = require('vue');
-
-var vm = new Vue({
-	el: "body",
-	components:{
-		index:Index
-	}
-});
