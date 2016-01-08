a7cf8c2dee61e600d1d603a4c83c1aafa9cc0acb
Lxxyx <841380530@qq.com>
2016-01-08 18:46:25 +0800

    add banner

--- public/app/components/slide.vue
+++ public/app/components/slide.vue
@@ -3,10 +3,8 @@
         <div class="am-u-sm-12 am-u-sm-centered" style="padding-left: 0!important;padding-right: 0!important;">
             <div class="am-slider am-slider-default" data-am-flexslider id="slider">
                 <ul class="am-slides">
-                    <li><img src="http://7xk109.com1.z0.glb.clouddn.com/NcuSpmslide.jpg" class="am-img-responsive" /></li>
-                    <li><img src="http://7xk109.com1.z0.glb.clouddn.com/NcuSpmslide.jpg" class="am-img-responsive" /></li>
-                    <li><img src="http://7xk109.com1.z0.glb.clouddn.com/NcuSpmslide.jpg" class="am-img-responsive" /></li>
-                    <li><img src="http://7xk109.com1.z0.glb.clouddn.com/NcuSpmslide.jpg" class="am-img-responsive" /></li>
+                    <li><img src="http://7xpour.com1.z0.glb.clouddn.com/banner1.png" class="am-img-responsive" /></li>
+                    <li><img src="http://7xpour.com1.z0.glb.clouddn.com/banner2.jpg" class="am-img-responsive" /></li>
                 </ul>
                 <ul class="am-direction-nav"></ul>
             </div>
