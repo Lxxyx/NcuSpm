d6d1d42388661078172048d1699fc89001f9607f
Lxxyx <841380530@qq.com>
2016-01-13 15:06:55 +0800

    add baidu analytics

--- public/app/components/index.vue
+++ public/app/components/index.vue
@@ -22,12 +22,14 @@ var Slide = require('./slide.vue');
 var News = require('./news.vue');
 var Bottom = require('./bottom.vue');
 var Vue = require('vue');
+
 Vue.filter('title', function(value) {
     if (value.length > 18) {
         value = value.substring(0, 18) + ".."
     };
     return value;
 });
+
 export default {
     components: {
         headernav: Header,
