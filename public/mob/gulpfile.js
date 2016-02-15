f54e1e491b7ca81d36f6333c2f1be03be4d6e3f4
Lxxyx <841380530@qq.com>
2016-02-15 20:14:34 +0800

    add resume

--- public/mob/gulpfile.js
+++ public/mob/gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('sass', function() {
-  return gulp.src('src/css/*.scss')
+gulp.task('resume-sass', function() {
+  return gulp.src('src/css/resume.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
@@ -12,10 +12,22 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('sass:watch', function() {
-  gulp.watch('./src/css/*.scss', ['sass']);
+gulp.task('icon-sass', function() {
+  return gulp.src('src/css/iconfont.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dist/iconfont/'));
 });
 
-gulp.task('default', ['sass', 'sass:watch'], function() {
+gulp.task('sass:watch', function() {
+  gulp.watch('./src/css/resume.scss', ['resume-sass']);
+  gulp.watch('./src/css/iconfont.scss', ['icon-sass']);
+  gulp.watch('./src/css/components/*.scss', ['resume-sass']);
+});
+
+gulp.task('default', ['resume-sass', 'icon-sass', 'sass:watch'], function() {
   console.log('Start watch sass')
 });
